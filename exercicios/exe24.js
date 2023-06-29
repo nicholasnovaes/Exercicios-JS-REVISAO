72d0571c813252400793c03d62b45a47480930d7
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-06-29 17:01:03 -0300

    exe 21, 22, 23, 24, 25

---
+++ exercicios/exe24.js
@@ -0,0 +1,12 @@
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.*/
+
+function hello(){
+    i = 0
+
+while(i < 12){
+    console.log('Hello World!!')
+    i++
+}
+}
+
+hello()
