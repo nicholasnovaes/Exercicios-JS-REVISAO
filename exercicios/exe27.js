5f891a09bd6f5ff1217f9ac94b2735da63c9064b
Nicholas Novaes <nicholasnovaesaraujo@gmail.com>
2023-06-30 16:20:15 -0300

    exe 26,27

---
+++ exercicios/exe27.js
@@ -0,0 +1,9 @@
+/*27) Construa uma função que receba como parâmetros as alturas e as taxas de crescimento anuais de duas
+crianças e calcule se existe uma criança menor, caso exista se a criança menor ultrapassará a maior e em
+quantos anos isso acontecerá. Utilize centímetros para as unidades de medida.*/
+
+function crescimento(altura1, taxa1, altura2, taxa2){
+    if(altura1 == altura2 && taxa1 == taxa2){
+        return (console.log('As crianças possuem altura e taxa de cresicmento iguais'))
+    }else if()
+}
